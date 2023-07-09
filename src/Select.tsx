ef39d4d9dd3988db03b5d43b4be2040b8f58e071
vinicius <vmm_7@hotmail.com>
2023-07-09 15:21:50 -0300

    useState para mostrar ou não as 'options'

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./select.module.css";
 
 type SelectOption = {
@@ -12,13 +13,15 @@ type SelectProps = {
 };
 
 export default function Select({ value, onChange, options }: SelectProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div tabIndex={0} className={styles.container}>
       <span className={styles.value}>{value?.label}</span>
       <button className={styles["clear-btn"]}>&times;</button>
       <div className={styles.divider}></div>
       <div className={styles.caret}></div>
-      <ul className={`${styles.options} ${styles.show}`}>
+      <ul className={`${styles.options} ${isOpen ? styles.show : ""}`}>
         {options.map(option => (
           <li key={option.label} className={styles.option}>
             {option.label}
